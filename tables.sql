b622336960dddcb7b7f77a180a2042264d528f6e
Pavel Tyukin <paveltyukinofficial@gmail.com>
2020-03-14 18:42:33 +0300

    change charset

--- tables.sql
+++ tables.sql
@@ -1,45 +1,39 @@
+CREATE SCHEMA online_game2 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-CREATE SCHEMA `online_game` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE online_game2;
 
-USE online_game;
-
-CREATE TABLE `online_game`.`users` (
-  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `user_password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `user_register` datetime DEFAULT current_timestamp(),
-  `user_telephone` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`user_id`)
+CREATE TABLE online_game2.users (
+	`user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`user_name` VARCHAR(100) NOT NULL,
+	`user_password` VARCHAR(100) NOT NULL,
+	`user_register` DATETIME DEFAULT CURRENT_TIMESTAMP,
+	`user_telephone` VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE `online_game`.`profiles` (
-  `profile_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) unsigned NOT NULL,
-  `p_first_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `p_last_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `p_photo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `p_gender` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `p_birthday` datetime NOT NULL,
-	PRIMARY KEY (`profile_id`),
+CREATE TABLE online_game2.profiles (
+	`profile_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`user_id` INT UNSIGNED NOT NULL,
+	`p_first_name` VARCHAR(100) NOT NULL,
+	`p_last_name` VARCHAR(100) NOT NULL,
+	`p_photo` VARCHAR(100) NOT NULL,
+	`p_gender` VARCHAR(100) NOT NULL,
+	`p_birthday` DATETIME NOT NULL,
 	CONSTRAINT fk_profiles_users FOREIGN KEY (user_id)
 		REFERENCES users (user_id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
 );
 
-CREATE TABLE `online_game`.`heroes` (
-	`hero_id` INT NOT NULL,
+CREATE TABLE online_game2.heroes (
+	`hero_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`hero_name` VARCHAR(100) NOT NULL,
-	`profile_id` INT NOT NULL,
+	`profile_id` INT UNSIGNED NOT NULL,
 	`hero_photo` VARCHAR(100) NOT NULL,
 	`hero_type_id` INT NOT NULL,
-    `hero_gender` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-	`hero_start` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`hero_id`),
+	`hero_gender` VARCHAR(100) NOT NULL,
+	`hero_start` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT fk_heroes_profiles FOREIGN KEY (profile_id)
 		REFERENCES profiles (profile_id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
 );
-
-
