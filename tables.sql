44cdd208070132638f92a32fe7680d6f02f706eb
Pavel Tyukin <paveltyukinofficial@gmail.com>
2020-03-14 18:02:16 +0300

    add not null

--- tables.sql
+++ tables.sql
@@ -4,20 +4,22 @@ CREATE SCHEMA `online_game` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE online_game;
 
 CREATE TABLE `online_game`.`users` (
-  `user_id` INT NOT NULL,
-  `user_name` VARCHAR(100) NULL DEFAULT NULL,
-  `user_password` VARCHAR(100) NULL DEFAULT NULL,
-  `user_register` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_telephone` VARCHAR(30) NULL DEFAULT NULL,
+  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `user_password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `user_register` datetime DEFAULT current_timestamp(),
+  `user_telephone` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`user_id`)
 );
 
 CREATE TABLE `online_game`.`profiles` (
-	`profile_id` INT NOT NULL,
-	`user_id` INT NOT NULL,
-	`p_first_name` VARCHAR(100) NOT NULL,
-	`p_last_name` VARCHAR(100) NOT NULL,
-	`p_photo` VARCHAR(100) NOT NULL,
+  `profile_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `p_first_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `p_last_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `p_photo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `p_gender` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `p_birthday` datetime NOT NULL,
 	PRIMARY KEY (`profile_id`),
 	CONSTRAINT fk_profiles_users FOREIGN KEY (user_id)
 		REFERENCES users (user_id)
